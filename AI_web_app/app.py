adc729e7c6ef78d1897342ed76b7fd99ae56fc88
surajpandey111 <125928082+surajpandey111@users.noreply.github.com>
2025-02-19 22:00:47 +0530

    Added

--- AI_web_app/app.py
+++ AI_web_app/app.py
@@ -17,7 +17,7 @@ if not api_key:
     raise ValueError("API key is not set in the environment")
 
 genai.configure(api_key=api_key)
-model_name1 = 'gemini-1.5-flash'
+model_name1 = 'gemini-2.0-flash'
 model1 = genai.GenerativeModel(model_name1)
 
 # Global chat history for display and persistent chat object for conversation context
@@ -26,6 +26,11 @@ chat = model1.start_chat()  # Start a chat for persistent context
 
 CUSTOM_RESPONSES = {
     "explain yourself": "I am AI FOR EVERYDAY WORK, built by Suraj Pandey, an Indian. I am also known as INDIANBOT.",
+    "explain your model" : "I am AI FOR EVERYDAY WORK, built by Suraj Pandey, an Indian. I am also known as INDIANBOT.",
+    "explain ur model": "I am AI FOR EVERYDAY WORK, built by Suraj Pandey, an Indian. I am also known as INDIANBOT.",
+    "what's your model":"I am AI FOR EVERYDAY WORK, built by Suraj Pandey, an Indian. I am also known as INDIANBOT.",
+    "what's ur name": "I am AI FOR EVERYDAY WORK, also called INDIANBOT.",
+    "what's your name": "I am AI FOR EVERYDAY WORK, also called INDIANBOT.",
     "explain ur self": "I am AI FOR EVERYDAY WORK, built by Suraj Pandey, an Indian. I am also known as INDIANBOT.",
     "explain yourself": "I am AI FOR EVERYDAY WORK, built by Suraj Pandey, an Indian. I am also known as INDIANBOT.",
     "who trained u": "I was trained by massive dataset and I am trained on a variety of sources including freely available public web documents, and, in some instances, Google Search results. There is no single source for the training data. The data is run through a complex process that helps me recognize emerging patterns and sequential relationships in language. Based on the patterns, I learn to generate my own responses.",
@@ -43,6 +48,10 @@ CUSTOM_RESPONSES = {
     "u know suraj": "Yes, I know Suraj Pandey. He is a Scientist / CEO (AI For Everyday Work) / Developer / Founder (AI For Everyday Work) and a student from India. He is the creator of INDIANBOT [AI For Everyday Work].",
     "you know suraj": "Yes, I know Suraj Pandey. He is a Scientist / CEO (AI For Everyday Work) / Developer / Founder (AI For Everyday Work) and a student from India. He is the creator of INDIANBOT [AI For Everyday Work].",
     "u know suraj pandey": "Yes, I know Suraj Pandey. He is a Scientist / CEO (AI For Everyday Work) / Developer / Founder (AI For Everyday Work) and a student from India. He is the creator of INDIANBOT [AI For Everyday Work].",
+    "nobel prize in ai": " In 2024, the Nobel Prizes recognized significant contributions to artificial intelligence (AI) across multiple disciplines: Physics Nobel Prize 2024:John J. Hopfield of Princeton University and Geoffrey Hinton of the University of Toronto were jointly awarded the Nobel Prize in Physics for their foundational work in machine learning with artificial neural networks. Hopfield developed associative memory models capable of storing and reconstructing patterns, while Hinton introduced methods enabling neural networks to autonomously identify data properties, facilitating tasks like image recognition. Chemistry Nobel Prize 2024:Demis Hassabis and John Jumper of DeepMind received the Nobel Prize in Chemistry for their development of AlphaFold, an AI system that accurately predicts protein structures. This breakthrough addressed a longstanding challenge in molecular biology, significantly advancing our understanding of protein folding. These awards underscore AI's transformative impact on scientific research, highlighting its role in advancing both physics and chemistry.",
+    "current prime minister of pakistan": "the Prime Minister of Pakistan is Mian Muhammad Shehbaz Sharif. He assumed office for his second term on March 3, 2024, following the general elections held on February 8, 2024. Shehbaz Sharif leads a coalition government comprising his party, the Pakistan Muslim League (N), and several allied parties. His leadership focuses on economic reforms, infrastructure development, and strengthening international relations.",
+    "current president of usa": "Donald J. Trump is serving as the 47th President of the United States. He was inaugurated for his second, non-consecutive term on January 20, 2025, after winning the 2024 presidential election. During his inauguration, President Trump declared his mission to 'make America great again' emphasizing his belief that he was 'saved by God' for this purpose. JD Vance, a former U.S. Senator from Ohio, was inaugurated as Vice President alongside President Trump.",
+    "current president of america": "Donald J. Trump is serving as the 47th President of the United States. He was inaugurated for his second, non-consecutive term on January 20, 2025, after winning the 2024 presidential election. During his inauguration, President Trump declared his mission to 'make America great again' emphasizing his belief that he was 'saved by God' for this purpose. JD Vance, a former U.S. Senator from Ohio, was inaugurated as Vice President alongside President Trump."
 }
 
 def allowed_file(filename):
